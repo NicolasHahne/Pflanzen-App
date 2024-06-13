62aecdda3a0fb19a44cb16da186aab67dbaec3e7
NicolasHahne <mail.nicolashahne@gmail.com>
2024-06-13 14:17:48 +0200

    Pflanzen-App basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BackgroundimageComponent } from './backgroundimage/backgroundimage.component';
+import { TaskbarComponent } from './taskbar/taskbar.component';
+import { HeaderComponent } from './header/header.component';
+import { BurgermenuComponent } from './burgermenu/burgermenu.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BackgroundimageComponent, TaskbarComponent, HeaderComponent, BurgermenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
